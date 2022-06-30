7ff149178afaf65363d7d2d0d1d7744b9b687b62
Marya Kim <markim1815@gmail.com>
2022-06-30 13:59:02 -0400

    Implemented task-related CRUD functions

--- habits/main.py
+++ habits/main.py
@@ -1,7 +1,7 @@
 #from . import models, schemas
 import models, schemas
 import list_controller, task_controller
-from database import SessionLocal, engine 
+from database import SessionLocal, engine
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
@@ -23,13 +23,11 @@ def get_db():
 def create_list(list: schemas.ListCreate, db: Session = Depends(get_db)):
     return list_controller.create_list(db = db, list = list)
 
-
 """Get all lists."""
 @app.get("/lists", response_model = List[schemas.List])
 def read_list(db: Session = Depends(get_db)):
     return list_controller.get_all_lists(db=db)
   
-
 """Update list given list_id."""
 @app.patch("/lists/{list_id}", response_model = schemas.List)
 def update_list(list_id: int, new_data: schemas.List, db: Session = Depends(get_db)):
@@ -41,18 +39,33 @@ def delete_list(list_id: int, db: Session = Depends(get_db)):
     return list_controller.delete_list(db=db, list_id=list_id)
 
 
-
 """Create new task given list id."""
+@app.post("/lists/{list_id}/tasks", response_model = schemas.Task)
+def add_task(task: schemas.TaskCreate, db: Session = Depends(get_db)):
+    return task_controller.add_task(task=task, db=db)
 
 """Get tasks associated with given list id."""
+@app.get("/lists/{list_id}/tasks", response_model = List[schemas.Task])
+def get_task(list_id: int, db: Session = Depends(get_db)):
+    return task_controller.get_list_tasks(db=db, list_id=list_id)
 
-"""Update task description, order in list, completion"""
+"""Update task description, order in list, or completion"""
+@app.patch("/lists/tasks/{id}", response_model = schemas.Task)
+def update_task(new_data: schemas.Task, db: Session = Depends(get_db)):
+    return task_controller.update_task(db=db, new_data=new_data)
 
 """Delete task from given list id."""
+@app.delete("/lists/tasks/{id}", response_model = schemas.Task)
+def delete_task(id: int, db: Session = Depends(get_db)):
+    return task_controller.delete_task(db=db, id=id)
 
-@app.post("/lists/drop/")
+
+"""Debug: Delete all lists."""
+@app.post("/lists/drop")
 def drop_all_lists(db: Session = Depends(get_db)):
-    pass
+    #db.drop(models.List)
+    db.delete
+    return True
 
  
 ##if __name__ == "__main__":
